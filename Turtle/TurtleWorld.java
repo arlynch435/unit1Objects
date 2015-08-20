c8e973fbcc82a83b9912f552c516e57e6c6efa2e
arlynch435 <arlynch@NN123-HA-04.HS.sd203.org>
2015-08-20 14:12:08 -0500

    Tried to make a triangle. Cried instead.

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -6,8 +6,13 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
+        turtle.setPenColor(Color.ORANGE);
         turtle.penDown();
+        turtle.turn(60);
         turtle.forward(50);
+        turtle.turn(60);
+        turtle.forward(87);
+        turtle.turn(150);
+       turtle.forward(100);
     }
 }
