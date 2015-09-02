1f65d4c6201316f1c39032ef0f2035775a34d7c2
arlynch435 <arlynch@NN123-HA-04.HS.sd203.org>
2015-09-02 14:13:21 -0500

    Turtle lab part 2

--- Chapter 2 practice/RandomTest.java
+++ Chapter 2 practice/RandomTest.java
@@ -3,8 +3,14 @@ public class RandomTest
 {
     public static void main(String[] args)
     {
+        // option A
         Random generator= new Random();
         int hello=generator.nextInt(6)+1;
         System.out.println(hello);
+        // option B
+        double goodbye=Math.random(); // returns a double number between 0 and 1
+        goodbye*=6; // range [0,6)
+        goodbye+=1; // range[1,7)
+        System.out.println((int)goodbye);//prints 1-6
     }
 }
--- TurtleLab/TurtleFun.java
+++ TurtleLab/TurtleFun.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.util.Random;
 
 public class TurtleFun
 {
@@ -40,9 +41,26 @@ public class TurtleFun
        omega.turn(360-45);
        blu.turn(180);
        omega.turn(180);
-       blu.forward(50);
-       omega.forward(150);
+       blu.penUp();
+       omega.penUp();
+       blu.forward(200);
+       omega.forward(300);
        blu.turn(90);
        omega.turn(90);
+       blu.turn(45);
+       blu.penDown();
+       omega.penDown();
+      Random generator= new Random();
+      int  thingie=generator.nextInt(360);
+      System.out.println(thingie); // remember 105,
+      int stuff=0;
+      while (stuff<100)     {
+          blu.turn(thingie);
+          omega.turn(0-thingie);
+          // thingie=generator.nextInt(100);
+          blu.forward(50);
+          omega.forward(50);
+          stuff+=1;
+        }
     }
 }
